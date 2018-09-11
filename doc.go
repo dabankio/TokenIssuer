52d79a81cf2503f87982acfbfab4996c986ea9d4
nanmu42 <i@nanmu.me>
2018-09-11 13:02:29 +0800

    [doc] update godoc

--- doc.go
+++ doc.go
@@ -1,4 +1,6 @@
 // package TokenIssuer -- Issue Your ERC-20 Token in a Minute
+//
+// details: https://github.com/dabankio/TokenIssuer
 package TokenIssuer
 
 //go:generate solc -o contract --abi --bin --optimize --overwrite contract/token.sol
